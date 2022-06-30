c36e0aa90797128b81266c60acab9edec59e3e4e
mostafaahmedemam <mostafaemam19597@gmail.com>
2022-06-30 13:10:30 +0200

    Enhance UI

--- reactivities/client-app/src/App.tsx
+++ reactivities/client-app/src/App.tsx
@@ -2,25 +2,25 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-
+import {Header, List} from "semantic-ui-react";
 function App() {
   const [activities,setActivities]=useState([]);
-  useEffect(()=> {axios.get('http://localgost:9000/api/activities').then(response=>{
+  useEffect(()=> {axios.get('http://localhost:9000/api/activities').then(response=>{
     console.log(response);
     setActivities(response.data);
   })
   },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <ul>
-          {activities.map((activity:any)=> (
-            <li key={activity.id}>
+    <div>
+      <Header as='h2' icon='users' content='reactivities'/>
+      
+      <List>    
+        {activities.map((activity:any)=> (
+            <List.Item key={activity.id}>
               {activity.title}
-            </li>
-          ))}
-        </ul>
-      </header>
+            </List.Item>
+        ))}
+      </List>
     </div>
   );
 }
