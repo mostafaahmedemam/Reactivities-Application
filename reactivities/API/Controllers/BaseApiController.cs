47dfc341ca720b521ad4cd955fecb2ef3af6b7d5
mostafaahmedemam <mostafaemam19597@gmail.com>
2022-07-01 15:34:37 +0200

    adding CRUD operation to backend using CQRS

--- reactivities/API/Controllers/BaseApiController.cs
+++ reactivities/API/Controllers/BaseApiController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
+using MediatR;
 namespace API.Controllers
 {
 
@@ -13,6 +13,8 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator??= HttpContext.RequestServices.GetService<IMediator>();  
         
     }
 }
